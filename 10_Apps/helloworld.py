31aaa24eaf04d4c802b93c63f6f9df0cdbb74b67
Francisco De La Cruz <helloquico@gmail.com>
2018-07-07 19:26:40 -0400

    Hello (you Pythonic) World

---
+++ 10_Apps/helloworld.py
@@ -0,0 +1,7 @@
+print("----------------------------------------------")
+print("                  HELLO WORLD                 ")
+print("----------------------------------------------")
+print()
+
+name = input("What is your name? ")
+print(f"Nice to meet you {name}")
