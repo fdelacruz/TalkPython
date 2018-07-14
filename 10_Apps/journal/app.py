caa88515995dc6f337a023a49d1fd750bccb912d
Francisco De La Cruz <helloquico@gmail.com>
2018-07-14 13:59:51 -0400

    Lists and for-in loops

--- 10_Apps/journal/app.py
+++ 10_Apps/journal/app.py
@@ -14,26 +14,32 @@ def run_event_loop():
 
     print("What do you want to do with your journal?")
     cmd = None
+    journal_data = []
 
     while cmd != "x":
         cmd = input("[L]ist entries, [A]dd an entry, E[x]it: ")
 
         if cmd.lower().strip() == "l":
-            list_stries()
+            list_stries(journal_data)
         elif cmd.lower().strip() == "a":
-            add_entry()
+            add_entry(journal_data)
         elif cmd.lower().strip() != "x":
             print(f"Sorry, we don't understand '{cmd}'.")
 
     print("Good bye!")
 
 
-def list_stries():
-    print("Listing..")
+def list_stries(data):
+    print("Your journal entries: ")
+    entries = reversed(data)
+
+    for index, entry in enumerate(entries):
+        print(f"* [{index+1}] {entry}")
 
 
-def add_entry():
-    print("Adding entry..")
+def add_entry(data):
+    text = input("Type your entry, <Enter> ")
+    data.append(text)
 
 
 main()
