84d96072d95f8cb2c9d96abaec1b24754603a34b
Francisco De La Cruz <helloquico@gmail.com>
2018-08-07 17:28:06 -0400

    Download and write binary data

--- 10_Apps/LOLCatFactory/app.py
+++ 10_Apps/LOLCatFactory/app.py
@@ -31,10 +31,14 @@ def get_or_create_output_folder():
 
 
 def download_cats(folder):
+    print('Contacting server to download cats..')
     cat_count = 8
     for i in range(1, cat_count + 1):
         name = f'lolcat {i}'
+        print('Downloading cat ' +name)
         cat_service.get_cat(folder, name)
+
+    print('Done.')
 
 
 if __name__ == "__main__":
