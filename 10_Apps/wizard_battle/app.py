c1dd124450bd354c5f78ed2ace79bf1245a4ddc0
Francisco De La Cruz <helloquico@gmail.com>
2018-08-11 12:33:05 -0400

    Wizard Battle - Build Game Loop

---
+++ 10_Apps/wizard_battle/app.py
@@ -0,0 +1,29 @@
+def main():
+    print_header()
+    game_loop()
+
+
+def print_header():
+    print('------------------------')
+    print('      WIZARD GAME       ')
+    print('------------------------')
+    print()
+
+
+def game_loop():
+    while True:
+
+        cmd = input('Do you [a]ttack, [r]un away, [l]ook around: ')
+        if cmd == 'a':
+            print('attack')
+        elif cmd == 'r':
+            print('run away')
+        elif cmd == 'l':
+            print('look around')
+        else:
+            print('OK, exiting game...bye!!')
+            break
+
+
+if __name__ == "__main__":
+    main()
