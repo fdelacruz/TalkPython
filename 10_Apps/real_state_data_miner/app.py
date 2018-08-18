64756f98148206b05f647ba340cc116ab9816c4b
Francisco De La Cruz <helloquico@gmail.com>
2018-08-18 14:46:58 -0400

    CSV Processing with the CSV module

--- 10_Apps/real_state_data_miner/app.py
+++ 10_Apps/real_state_data_miner/app.py
@@ -1,4 +1,5 @@
 import os
+import csv
 
 
 def main():
@@ -20,18 +21,34 @@ def get_data_file():
     return os.path.join(base_folder, 'data',
                         'SacramentoRealEstateTransactions2008.csv')
 
-
 def load_file(filename):
     with open(filename, 'r', encoding='utf-8') as fin:
-        header = fin.readline().strip()
-        print('found header: ' + header)
 
-        lines = []
-        for line in fin:
-            line_data = line.strip().split(',')
-            lines.append(line_data)
+        reader = csv.DictReader(fin)
+        for row in reader:
+            print(type(row), row)
+            print('Bed count {}:'.format(row['beds']))
 
-        print(lines[:5])
+        # header = fin.readline().strip()
+        # reader = csv.reader(fin, delimiter=',')
+        # print(header.upper())
+        # for row in reader:
+        #     print(type(row), row)
+        #     beds = row[4]
+        #     print('Bed count {}:'.format(beds))
+
+
+# def load_file(filename):
+#     with open(filename, 'r', encoding='utf-8') as fin:
+#         header = fin.readline().strip()
+#         print('found header: ' + header)
+#
+#         lines = []
+#         for line in fin:
+#             line_data = line.strip().split(',')
+#             lines.append(line_data)
+#
+#         print(lines[:5])
 
 
 def query_data(data):
